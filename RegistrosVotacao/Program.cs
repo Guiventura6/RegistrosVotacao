764050160d95fab6c9063bb09670777117ae49ff
Guilherme Ventura <guilhermeventurasousa@gmail.com>
2022-01-07 11:37:12 -0300

    Implementacao da leitura dos dados e processamento do resultado no arquivo program.

--- RegistrosVotacao/Program.cs
+++ RegistrosVotacao/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace RegistrosVotacao
 {
@@ -6,7 +8,35 @@ namespace RegistrosVotacao
     {
         static void Main(string[] args)
         {
-            
+            Dictionary<string, int> candidatos = new Dictionary<string, int>();
+
+            Console.Write("Enter file full path: ");
+            string path = Console.ReadLine();
+
+            try
+            {
+                using StreamReader sr = File.OpenText(path);
+                while (!sr.EndOfStream)
+                {
+                    string[] line = sr.ReadLine().Split(',');
+                    string NomeCandidato = line[0];
+                    int votos = int.Parse(line[1]);
+                    
+                    if (candidatos.ContainsKey(NomeCandidato))                                            
+                        candidatos[NomeCandidato] += votos;                    
+                    else                    
+                        candidatos.Add(NomeCandidato, votos);                   
+                }                                
+                Console.WriteLine("RESULTADO!");                
+                foreach (var item in candidatos)
+                {
+                    Console.WriteLine($"{item.Key}: {item.Value}");
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
